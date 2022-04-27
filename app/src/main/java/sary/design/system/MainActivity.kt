e92dee5588d598493f13a56970f302948fe574b3
omarAbdElsameaa <o.abdelsamea@sary.com>
2022-04-27 13:29:10 +0200

    Initial commit

---
+++ app/src/main/java/sary/design/system/MainActivity.kt
@@ -0,0 +1,13 @@
+package sary.design.system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sary.design.buttons.LoadingButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ buttons/src/main/java/sary/design/buttons/ButtonState.kt
@@ -0,0 +1,7 @@
+package sary.design.buttons
+
+sealed class ButtonState() {
+    object Clicked : ButtonState()   // when button is clicked for downloading
+    object Loading : ButtonState()   // when downloading is in progress
+    object Completed : ButtonState() // when downloading is finished
+}
